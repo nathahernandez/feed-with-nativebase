29c67672a008892cc5fda9bcf57f21c7973d3147
nhcorrea <nhcorrea@debian>
2023-04-12 14:38:28 -0300

    feat: add home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Box } from 'native-base'
+
+import Feed from '../../components/organisms/Feed'
+import Stories from '../../components/organisms/Stories'
+import Header from '../../components/organisms/Header'
+
+export default function Home() {
+  return (
+    <Box flex={1} backgroundColor="#fff" flexDirection="column">
+      <Header />
+      <Stories />
+      <Feed />
+    </Box>
+  )
+}
