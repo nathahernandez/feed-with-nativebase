6d44902fe4e4da7fd5278fdd7f5e34a8d380b068
nhcorrea <nhcorrea@debian>
2023-04-12 14:41:42 -0300

    feat: initial structure created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { LinearGradient } from 'expo-linear-gradient'
+
+import Home from './src/screens/Home'
+
+const config = {
+  dependencies: {
+    'linear-gradient': LinearGradient,
+  },
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider config={config}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Home />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NativeBaseProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
